96681f73208696cea663131c8842fd6699ca9cd9
mirfou <fourmitux111@gmail.com>
2024-06-20 21:45:00 -0400

    Service de mailing avec spring boot

---
+++ src/test/java/kilif/learn/mailsending/MailSendingApplicationTests.java
@@ -0,0 +1,13 @@
+package kilif.learn.mailsending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSendingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
